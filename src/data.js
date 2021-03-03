3d0abc5cad88369be94aa53f80084e1c42229005
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-03 10:24:52 +0100

    wersje export

---
+++ src/data.js
@@ -0,0 +1,28 @@
+// zmienne
+// export wersja pierwsza pojedynczy
+// export const name = "john";
+// export const age = 27;
+
+// objekt
+// export const person = {
+//   name: "peter",
+//   age: 25,
+// };
+
+// const secretValue = "secret";
+
+
+// export wersja druga wszystkie wpisane
+// zmienne
+const name = "john";
+const age = 27;
+
+// objekt
+const person = {
+  name: "peter",
+  age: 25,
+};
+
+const secretValue = "secret";
+
+export { name, age, person };
