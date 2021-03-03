da965125186b2653893c5262100bc709b1ca6daf
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-03 18:54:25 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-// named and default import/export
-// only one default export module
-// think of module as file
+// named and default import/export - nazwany i domyślny import/eksport
+// nazwany i domyślny import/eksport
+// only one default export module - tylko jeden domyślny moduł eksportowy
+// think of module as file - pomyśl o module jak o pliku
+// stateless functional components- bezstanowe elementy funkcjonalne
+// class based components - komponenty oparte na klasach
+// state, life cycle methods - stan, metody cyklu życia
+// React Hooks - haki react
+// Classes-syntactical sugar for constructor functions - Klasy - składniowy środek pomocniczy dla funkcji konstruktora
+// subclass of components - podklasa elementów
+// extends from component class -rozszerza się z klasy komponentu
+// inherits functionality - dziedziczy funkcjonalność
+// component class is used to transfer from basic E6 class - klasa składowa służy do przenoszenia z klasy podstawowej E6
+// state, this.setState = hold/access/change data of component - stan, ten.ustawStan = trzymać / dostęp / zmiana danych komponentu
+// props = read only - rekwizyty = tylko do odczytu
 
 import React from "react";
-import { name, age, person } from "./data";
-import Banner from './component/Header/Banner'
+import "./index.css";
+import BookList from "./BookList"
+// import { render } from "@testing-library/react";
 
 const App = () => {
   return (
-    <>
     <section>
-    <Banner/>
-      <p>this is my content</p>
-      <p>{name}</p>
-      <p>{age}</p>
-      <p>{person.name}</p>
+      <h1>This is our Application</h1>
+      <BookList/>
     </section>
-    </>
   );
 };
+
+// class App1 extends Component {
+//   render() {
+//     return (
+//       <section>
+//         <Banner />
+//         <p>this is my content</p>
+//         <p>{name}</p>
+//         <p>{age}</p>
+//         <p>{person.name}</p>
+//       </section>
+//     );
+//   }
+// }
+export default App;
 
 // import calej daty
 // import * as data from './data'
@@ -32,12 +55,3 @@ const App = () => {
 //     </section>
 //   );
 // };
-
-
-
-
-
-
-
-
-export default App;
