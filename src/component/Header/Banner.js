3698ea74f840b3d7fabe8155e2c5143bf60c98b2
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-03 10:55:00 +0100

    create Banner and different export version

---
+++ src/component/Header/Banner.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { name } from "../../data";
+// rfc
+// rcc
+
+// export default function Banner() {
+//   return (
+//     <div>
+//       <h1>Hello from Banner {name}</h1>
+//     </div>
+//   );
+// }
+
+// const Banner = () => {
+//     return (
+//         <div>
+//             <h1>Hello from Banner {name}</h1>
+//         </div>
+//     );
+// }
+// export default Banner;
+
+function Banner () {
+    return (
+        <div>
+            <h1>Hello from Banner {name}</h1>
+        </div>
+    );
+}
+export default Banner;
+
