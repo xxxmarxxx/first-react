ab0440805324269c5041f3ee04fea2ae336babf5
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-04 09:46:19 +0100

    add constructor,super

--- src/BookList.js
+++ src/BookList.js
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 import Book from "./Book";
+import BookData from './BookData';
 class BookList extends Component {
-
-books = [
-    {
-        book:'book number one',
-        author:'john doe'
-    },
-    {
-        book:'book number two',
-        author:'bobby doe'
-    },
-]
-
+    constructor(props){
+        super(props)
+        this.state = {
+            books: BookData
+        }
+    }
+//   state = {
+    
+//   };
+  // this.setState({})
   render() {
+    // const books = this.state.books.map((item) => item.book);
+    // console.log(books);
+    // filter
+    // forEach
+    // map
+    // reduce
     return (
       <section>
         <h3>This is our BookList</h3>
-        <Book book={this.books[0]}/>
+        {this.state.books.map((item) => (
+          <Book key={item.id} info={item}></Book>
+        ))}
       </section>
     );
   }
