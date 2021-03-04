4e73916e446118a25f3e861df90f0b2fb9db4180
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-04 09:45:25 +0100

    create BookData

---
+++ src/BookData.js
@@ -0,0 +1,28 @@
+const books = [
+  {
+    id: 1,
+    book: "book number one",
+    author: "john doe",
+  },
+  {
+    id: 2,
+    book: "book number two",
+    author: "bobby doe",
+  },
+  {
+    id: 3,
+    book: "book number three",
+    author: "mariusz doe",
+  },
+  {
+    id: 4,
+    book: "book number three",
+    author: "mariusz doe",
+  },
+  {
+    id: 5,
+    book: "book number three",
+    author: "mariusz doe",
+  },
+];
+export default books;
