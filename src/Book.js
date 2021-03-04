dd56e00f1aab95aeeaf79eb599d6acd746227834
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-04 09:45:05 +0100

    add const

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Book extends Component {
-    render() {
-        console.log(this.props);
-        return (
-            <article>
-                <h3>book :</h3>
-                <h3>author :</h3>
-            </article>
-        );
-    }
+  render() {
+    // console.log(this.props);
+    const { book, author } = this.props.info;
+    return (
+      <article>
+        <h3>book : {book}</h3>
+        <h3 className="author">author : {author} </h3>
+      </article>
+    );
+  }
 }
 
 export default Book;
