04800c0780d5826058230dd2a959123dcbfa9197
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-04 11:35:21 +0100

    handleClick

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 
 class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+    };
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+    //   handleClick() {
+    //     console.log("you clicked me");
+    //     console.log(this.state.count);
+    //   }
+
+    // wersja z funkcja strzalkowa
+handleClick = ()=> {
+    console.log('you clicked me');
+    console.log(this.state.count);
+}
+
   render() {
     // console.log(this.props);
-    const { book, author } = this.props.info;
+
+    const { img, title, author } = this.props.info;
     return (
-      <article>
-        <h3>book : {book}</h3>
-        <h3 className="author">author : {author} </h3>
+      <article className="book">
+        <img src={img} width="150" alt="book" />
+        <div>
+          <h3>Titel : {title}</h3>
+          <h3 className="author">Author : {author} </h3>
+          <button type="button" onClick={this.handleClick}>
+            add count
+          </button>
+        </div>
       </article>
     );
   }
