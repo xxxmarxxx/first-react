b287e2e85772f23de2c1e7107dc015a536e3e6aa
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-04 11:35:41 +0100

    new data book from wwww

--- src/BookData.js
+++ src/BookData.js
@@ -1,28 +1,77 @@
-const books = [
-  {
-    id: 1,
-    book: "book number one",
-    author: "john doe",
-  },
-  {
-    id: 2,
-    book: "book number two",
-    author: "bobby doe",
-  },
-  {
-    id: 3,
-    book: "book number three",
-    author: "mariusz doe",
-  },
-  {
-    id: 4,
-    book: "book number three",
-    author: "mariusz doe",
-  },
-  {
-    id: 5,
-    book: "book number three",
-    author: "mariusz doe",
-  },
-];
+const books  = [
+
+    {
+        id:1,
+        img:'https://images-na.ssl-images-amazon.com/images/I/51pMrW0+vcL._AC_SX368_.jpg',
+        title: 'Saint X',
+        author:'Alexis Schaitkin'
+    },
+    {
+        id:2,
+        img:'https://images-na.ssl-images-amazon.com/images/I/51NpGEKBQoL._AC_SX368_.jpg',
+        title:'The Cat in the Hat',
+        author:'Dr. Seuss'
+    },
+    {
+        id:3,
+        img:'https://images-na.ssl-images-amazon.com/images/I/51CDMBtlLgL._AC_SX368_.jpg',
+        title:'Fox in Socks',
+        author: 'Dr. Seuss'
+    }
+    ,
+    {
+        id:4,
+        img:'https://images-na.ssl-images-amazon.com/images/I/51CDMBtlLgL._AC_SX368_.jpg',
+        title:'Fox in Socks',
+        author: 'Dr. Seuss'
+    },
+    {
+        id:5,
+        img:'https://images-na.ssl-images-amazon.com/images/I/51NpGEKBQoL._AC_SX368_.jpg',
+        title:'The Cat in the Hat',
+        author:'Dr. Seuss'
+    },
+    {
+        id:6,
+        img:'https://images-na.ssl-images-amazon.com/images/I/51pMrW0+vcL._AC_SX368_.jpg',
+        title: 'Saint X',
+        author:'Alexis Schaitkin'
+    },
+]
+
 export default books;
+
+
+
+
+
+
+
+// const books = [
+//   {
+//     id: 1,
+//     book: "book number one",
+//     author: "john doe",
+//   },
+//   {
+//     id: 2,
+//     book: "book number two",
+//     author: "bobby doe",
+//   },
+//   {
+//     id: 3,
+//     book: "book number three",
+//     author: "mariusz doe",
+//   },
+//   {
+//     id: 4,
+//     book: "book number three",
+//     author: "mariusz doe",
+//   },
+//   {
+//     id: 5,
+//     book: "book number three",
+//     author: "mariusz doe",
+//   },
+// ];
+// export default books;
