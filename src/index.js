2cf46626a17fa1271fcd130de01f8c449233b595
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-05 10:29:51 +0100

    change for tutorial udemy 130

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Udemy from './UdemyLe130_hook'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Udemy />
   </React.StrictMode>,
   document.getElementById('root')
 );
