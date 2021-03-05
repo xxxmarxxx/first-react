efba04e082349411feee5302b994be359bbd770a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-05 10:54:31 +0100

    Lesson 132

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Udemy from './UdemyLe130_hook'
+// import Udemy from './UdemyLe130_hook';
+import Udemy2 from './UdemyLe132';
 
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Udemy />
+    <Udemy2 />
   </React.StrictMode>,
   document.getElementById('root')
 );
