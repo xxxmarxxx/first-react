944959dcec24a9475224fb5223852ed81003bad3
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-09 11:36:07 +0100

    create new file

---
+++ src/Links.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const links = () => {
+    const handleLink = ()=>{
+        const aaa = "https://sass-lang.com/documentation/at-rules/mixin";
+        
+    }
+    return ( 
+        <div>
+            <button className="btn" style={{margin:'2rem'}} onClick={handleLink}>link</button>
+            <a href="https://sass-lang.com/documentation/at-rules/mixin">sass</a>
+        </div>
+     );
+}
+ 
+export default links;
