897aa58177fd814ad4f10d11581704085247d749
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-10 21:47:43 +0100

    state and setState

--- src/PolskieUdemy/Hook.js
+++ src/PolskieUdemy/Hook.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import "../index.css";
 
 const App = () => {
+  const [state, setState] = useState({ a: 0, b: 10 });
+
+  const handler = () => setState({...state, a: state.a + 1});
   return (
-    <div>
-      <h1>Hello in react </h1>
-      <p>Start editing to see some magic happen!</p>
+    <div onClick={handler} style={{cursor:'pointer', textAlign:'center',margin:'200px'}}>
+      <h1>{state.a} </h1>
+      <p>{state.b}</p>
     </div>
   );
 };
