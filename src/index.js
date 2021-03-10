9dc084a0ff7da3d614252a40d617b01db92c2724
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-10 21:16:39 +0100

    add result

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import './index.css';
 // import Udemy from '.PolskieUdemy/UdemyLe130_hook';
 import Udemy2 from './PolskieUdemy/UdemyLe132';
 import Udemy135 from './PolskieUdemy/UdemyLe135';
+import Hook from './PolskieUdemy/Hook'
 
-
+const startValue=0;
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Udemy2 />
-    <Udemy135 />
+    {/* <Udemy2 /> */}
+    <Udemy135 result={startValue}/>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
