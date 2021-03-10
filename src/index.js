a627d43af1c61c94c5242f13a1f865b4c7dd9aec
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-10 18:17:14 +0100

    add new lesson

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 // import App from './App';
 // import Udemy from '.PolskieUdemy/UdemyLe130_hook';
 import Udemy2 from './PolskieUdemy/UdemyLe132';
+import Udemy135 from './PolskieUdemy/UdemyLe135';
 
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     <Udemy2 />
+    <Udemy135 />
   </React.StrictMode>,
   document.getElementById('root')
 );
