21fecdb70763dafc68ff0865934855e54a64ce38
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-10 21:17:09 +0100

    create new file LE 137

---
+++ src/PolskieUdemy/Hook.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello in react </h1>
+      <p>Start editing to see some magic happen!</p>
+    </div>
+  );
+};
+
+export default App;
