f75cf9e1390e99b34b91cfae600e15b7828ab409
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-11 11:11:27 +0100

    add components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 // import App from './App';
 // import Udemy from '.PolskieUdemy/UdemyLe130_hook';
-import Udemy2 from './PolskieUdemy/UdemyLe132';
-import Udemy135 from './PolskieUdemy/UdemyLe135';
-import Hook from './PolskieUdemy/Hook'
+// import Udemy2 from "./PolskieUdemy/UdemyLe132";
+// import Udemy135 from "./PolskieUdemy/UdemyLe135";
+import Hook from "./PolskieUdemy/Hook";
+import Hook2 from "./PolskieUdemy/Hook_2";
+import Hook3 from "./PolskieUdemy/Hook_3";
 
-const startValue=0;
+// const startValue = 0;
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Udemy2 /> */}
     {/* <Udemy135 result={startValue}/> */}
-    <Hook/>
+    <Hook />
+    <Hook2 />
+    <Hook3 />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
