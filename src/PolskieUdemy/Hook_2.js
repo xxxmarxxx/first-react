1cbc0105800ea9bccbb6900cb939a08ad07e4ae9
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-11 13:48:45 +0100

    cleaning

--- src/PolskieUdemy/Hook_2.js
+++ src/PolskieUdemy/Hook_2.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../index.css";
+
 
 // useState example 2
 
--- src/PolskieUdemy/Hook_3.js
+++ src/PolskieUdemy/Hook_3.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../index.css";
+
 
 // useState and useEffect
 
@@ -10,6 +10,9 @@ const App = () => {
   const handler = () => setCounter(counter + 1);
   const toggleVisibilityCounter = () => setIsActive((prevValue) => !prevValue);
 
+  const counterComponent = isActive ? (
+    <Counter rerenderCounter={counter} />
+  ) : null;
   //   useEffect(() => {
   //       alert('Hello')
   //   }, []);
@@ -21,24 +24,34 @@ const App = () => {
       <button className="btn" onClick={toggleVisibilityCounter}>
         on/off component
       </button>
-      <div style={{margin:'20px', backgroundColor:'red', color:'white'}}>{isActive && <Counter/>}</div>
+      <button className="btn_mar" onClick={handler}>
+        przerenderuj component
+      </button>
+      <div style={{ margin: "20px", backgroundColor: "red", color: "white" }}>
+        {counterComponent}
+      </div>
       <hr style={{ margin: "10px" }}></hr>
     </div>
   );
 };
 
-const Counter = () => {
+const Counter = ({ rerenderCounter }) => {
   const [counter, setCounter] = useState(0);
   const handleMouseMove = (event) => setCounter(event.clientX);
 
   useEffect(() => {
     window.addEventListener("mousemove", handleMouseMove);
-  }, []);
+
+    return () => {
+      alert("component unmounted ");
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [rerenderCounter]);
 
   return (
     <div>
       <p>{counter}</p>
-      {/* <p>{rerenderCounter}</p> */}
+      <p>{rerenderCounter}</p>
     </div>
   );
 };
