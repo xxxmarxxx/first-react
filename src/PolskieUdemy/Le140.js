f43f179b0e05735e801a6e5558d01f951f43f2c4
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-11 18:56:28 +0100

    create new file

---
+++ src/PolskieUdemy/Le140.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from "react";
+
+// jak korzystac z contex
+
+class App extends PureComponent {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz</h1>
+        <p>Start editing to se some magic happen </p>
+      </div>
+    );
+  }
+}
+
+export default App;
