eedc8c3b046c1f35931102df47445ae8b3d0bc66
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-11 11:15:18 +0100

    useState example1

--- src/PolskieUdemy/Hook.js
+++ src/PolskieUdemy/Hook.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import "../index.css";
 
+// useState example 1
+
 const App = () => {
-  const [state, setState] = useState({ a: 0, b: 10 });
+    const [state, setState] = useState({ a: 0, b: 10 });
 
-  const handler = () => setState({...state, a: state.a + 1});
-  return (
-    <div onClick={handler} style={{cursor:'pointer', textAlign:'center',margin:'200px'}}>
-      <h1>{state.a} </h1>
-      <p>{state.b}</p>
-    </div>
-  );
-};
+    const handler = () => setState({...state, a: state.a + 1});
+    return (
+      <div onClick={handler} style={{cursor:'pointer', textAlign:'center',margin:'100px'}}>
+        <hr style={{margin:'10px'}}></hr>
+        <h1>{state.a} </h1>
+        <p>{state.b}</p>
+        <hr style={{margin:'10px'}}></hr>
+      </div>
+    );
+  };
 
-export default App;
+  export default App;
+
+
+
