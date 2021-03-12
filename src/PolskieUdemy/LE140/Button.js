7cd474df6c70228452c986c1391ff9042dfa9ce9
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-12 11:08:08 +0100

    create new file

---
+++ src/PolskieUdemy/LE140/Button.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+
+class Button extends PureComponent {
+    state = {  }
+    render() { 
+        return ( 
+            <button className='btn_mar'>switch user status</button>
+         );
+    }
+}
+ 
+export default Button;
