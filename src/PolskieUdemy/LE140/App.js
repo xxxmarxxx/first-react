82a69c74faaac616dabb7a82eb9443eec58068ad
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-12 12:22:01 +0100

    added two appContext button

--- src/PolskieUdemy/LE140/App.js
+++ src/PolskieUdemy/LE140/App.js
@@ -10,30 +10,48 @@ import { AppContext, defaultObject } from "./AppContext";
 class App extends PureComponent {
   state = {
     isUserLogged: defaultObject.isUserLogged,
+    isUserAdult: true,
   };
 
   render() {
     return (
-      <div>
+      <div className='center'>
         <AppContext.Provider
           value={{
             isUserLogged: this.state.isUserLogged,
-            toggleLoggedState: this.handleToggleStateIsLogeged,
+            toggleLoggedState: this.handleToggleStateIsLogged,
           }}
         >
           <UserInfo />
           <Button />
+        </AppContext.Provider>
+        <AppContext.Provider
+          value={{
+            isUserLogged: this.state.isUserAdult,
+            toggleLoggedState: this.handleToggleStateIsAdult,
+          }}
+        >
+          <UserInfo />
           <Button />
         </AppContext.Provider>
-        <h1>Hello StackBlitz</h1>
-        <p>Start editing to se some magic happen </p>
+        <h1>on/off Logged</h1>
+        <p>Start editing to see some magic happen </p>
       </div>
     );
   }
-  handleToggleStateIsLogged = () =>
-    this.setState((prevState) => ({
-      isUserLogged: !prevState.isUserLogged,
+  handleToggleStateIsLogged = () => {
+    // console.log('hello');
+    this.setState(prevState => ({
+      isUserLogged: !prevState.isUserLogged
     }));
+  }
+  handleToggleStateIsAdult = () => {
+    
+    this.setState(prevState => ({
+      isUserAdult: !prevState.isUserAdult
+    }));
+  }
+  
 }
 
 export default App;
