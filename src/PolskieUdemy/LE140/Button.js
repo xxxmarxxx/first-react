ed5bafca9ef5bccd09cecc98286f0fd789f088fd
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-12 12:22:38 +0100

    add appContext.Consumer

--- src/PolskieUdemy/LE140/Button.js
+++ src/PolskieUdemy/LE140/Button.js
@@ -1,12 +1,19 @@
 import React, { PureComponent } from "react";
+import { AppContext } from "./AppContext";
 
 class Button extends PureComponent {
-    state = {  }
-    render() { 
-        return ( 
-            <button className='btn_mar'>switch user status</button>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <AppContext.Consumer>
+        {({toggleLoggedState}) => (
+          <button onClick={toggleLoggedState} className="btn_mar">
+            switch user status
+          </button>
+        )}
+      </AppContext.Consumer>
+    );
+  }
 }
- 
+
 export default Button;
