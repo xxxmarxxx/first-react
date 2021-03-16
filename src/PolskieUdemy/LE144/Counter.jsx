c06c2b8316c0ec8988ec60389439386b5a654cce
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-16 12:21:28 +0100

    change

--- src/PolskieUdemy/LE144/Counter.jsx
+++ src/PolskieUdemy/LE144/Counter.jsx
@@ -1,25 +1,32 @@
-import React, {PureComponent} from 'react';
+import React, { createRef, PureComponent } from "react";
 
 class Counter extends PureComponent {
-    state = { 
-        counter: 0,
-     }
-    render() { 
-        const {counter} = this.state;
-        return ( 
-            <div>
-                <p> The counter value is: {counter}</p>
-                <button className="btn_mar" onClick={this.asyncIncreaseCounterValue}>Get asynchronous data</button>
-            </div>
-         );
-    }
-    asyncIncreaseCounterValue = ()=> setTimeout(
-        ()=>{
-            this.setState(prevState => ({
-                counter: prevState.counter + 1,
+  state = {
+    counter: 0,
+  };
+
+componentRef = createRef()
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div ref={this.componentRef}>
+        <p> The counter value is: {counter}</p>
+        <button className="btn_mar" onClick={this.asyncIncreaseCounterValue}>
+          Get asynchronous data
+        </button>
+      </div>
+    );
+  }
+
+  asyncIncreaseCounterValue = () =>
+    setTimeout(() => {
+        if (this.componentRef.current){
+            this.setState((prevState) => ({
+              counter: prevState.counter + 1,
             }))
-        },1000
-    )
+        }
+    }, 3000);
 }
- 
+
 export default Counter;
