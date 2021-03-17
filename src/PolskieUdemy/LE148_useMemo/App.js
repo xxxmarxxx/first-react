da7950d33f36e88da126c9aca2fa3951989d0f65
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-17 17:49:10 +0100

    new option

--- src/PolskieUdemy/LE148_useMemo/App.js
+++ src/PolskieUdemy/LE148_useMemo/App.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
-import Counter from './Counter'
+import React, { useMemo, useState } from "react";
+import Counter from "./Counter";
 const App = () => {
+  const [state, setState] = useState({ x: 0, y: 0 });
   const [firstCounter, setFirstCounter] = useState(0);
   const [secondCounter, setSecondCounter] = useState(0);
+
+  const increaseX = () => setState({ ...state, x: state.x + 1 });
+  const increaseY = () => setState({ ...state, y: state.y + 1 });
 
   const increaseFirstCounter = () => setFirstCounter(firstCounter + 1);
 
   const increaseSecondCounter = () => setSecondCounter(secondCounter + 1);
 
+  // const firstCounterComponent = useMemo(()=>
+  // <Counter counter={firstCounter} index={1}/>,[firstCounter]);
+
   return (
     <div className="center">
       <h1>Counter</h1>
-      <p>counter no. 1: {firstCounter}</p>
-      <p>counter no. 2: {secondCounter}</p>
-      <Counter index={1}/>
-      <Counter index={2}/>
-      
-      <button onClick={increaseFirstCounter} className="btn_mar">add counter no.1 </button>
-      <button onClick={increaseSecondCounter} className="btn_mar">add counter no.2 </button>
+      <p>counter no. 1: {state.x}</p>
+      <p>counter no. 2: {state.y}</p>
+      {/* {firstCounterComponent} */}
+      <Counter counter={state} index={1} />
+      <Counter counter={state} index={2} />
+
+      <button onClick={increaseX} className="btn_mar">
+        add counter no.1{" "}
+      </button>
+      <button onClick={increaseY} className="btn_mar">
+        add counter no.2{" "}
+      </button>
     </div>
   );
 };
