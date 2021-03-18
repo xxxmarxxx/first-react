17449c48d72a959d3e71269907f95100a13ec96e
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-18 12:12:16 +0100

    add prevValue

--- src/PolskieUdemy/LE151_useCallback/App.js
+++ src/PolskieUdemy/LE151_useCallback/App.js
@@ -1,10 +1,10 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Counter from "./Counter";
 const App = () => {
   
   
-  const increaseFirstCounter = () => setFirstCounter(firstCounter + 1);
-  const increaseSecondCounter = () => setSecondCounter(secondCounter + 1);
+  const increaseFirstCounter = useCallback(() => setFirstCounter(prevValue => prevValue + 1),[]);
+  const increaseSecondCounter = useCallback(() => setSecondCounter(prevValue => prevValue + 1),[]);
   
   const [firstCounter, setFirstCounter] = useState(0);
   const [secondCounter, setSecondCounter] = useState(0);
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div className="center">
-      <h1>Counter</h1>
+      <h1>Counter / useCallback</h1>
       <p>counter no. 1: {firstCounter}</p>
       <p>counter no. 2: {secondCounter}</p>
       {firstCounterComponent}
