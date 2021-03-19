4771b5dfa35609b2682580fb0158f589c0be4aa5
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-19 11:15:20 +0100

    add button and handleOnClick

--- src/PolskieUdemy/LE153_useHistory/ReactJS.js
+++ src/PolskieUdemy/LE153_useHistory/ReactJS.js
@@ -1,11 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import { useHistory } from "react-router-dom";
 const ReactJS = () => {
-    return ( 
-        <article>
-            <h2>ReactJS</h2>
-        </article>
-     );
-}
- 
+  const history = useHistory();
+
+  const handleOnClick = () => {
+    const location = {
+      pathname: "redux",
+    };
+    history.push(location)
+  };
+  return (
+    <article>
+      <h2>ReactJS</h2>
+      <button onClick={handleOnClick} className="btn_mar">
+        idz do Redux
+      </button>
+    </article>
+  );
+};
+
 export default ReactJS;
