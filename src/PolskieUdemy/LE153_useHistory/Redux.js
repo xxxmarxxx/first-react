8d24fae526fe6cd1968b28518adfac8089570793
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-19 11:16:02 +0100

    add button and handle history.goBack()

--- src/PolskieUdemy/LE153_useHistory/Redux.js
+++ src/PolskieUdemy/LE153_useHistory/Redux.js
@@ -2,9 +2,13 @@ import React from 'react';
 import {useHistory} from 'react-router-dom'
 const Redux = () => {
     const history = useHistory();
+
+    const handleOnClick = ()=> history.goBack();
+    console.log(history);
     return ( 
         <article>
             <h2>Redux</h2>
+            <button onClick={handleOnClick} className='btn_mar'>wroc do poprzedniej strony</button>
         </article>
      );
 }
