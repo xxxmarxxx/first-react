6ff5d6c8fb7ac5693833e84db07134a234a74e0a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-19 10:58:38 +0100

    create Redux

---
+++ src/PolskieUdemy/LE153_useHistory/Redux.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom'
+const Redux = () => {
+    const history = useHistory();
+    return ( 
+        <article>
+            <h2>Redux</h2>
+        </article>
+     );
+}
+ 
+export default Redux;
