6141eafe7442d4c385a726e5a5d10b5c8a7ecebc
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-22 13:17:18 +0100

    add state

---
+++ src/PolskieUdemy/LE154_useLocation/ReactJS.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+const ReactJS = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => {
+    const location = {
+      pathname: "redux",
+      state: {
+        isActive: true,
+      },
+    };
+    history.push(location);
+  };
+  return (
+    <article>
+      <h2>ReactJS</h2>
+      <button onClick={handleOnClick} className="btn_mar">
+        idz do Redux
+      </button>
+    </article>
+  );
+};
+
+export default ReactJS;
