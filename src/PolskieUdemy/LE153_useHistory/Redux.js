0ab029f830e946ab5b4afa9b16ffa5957a3cddb8
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-22 12:58:09 +0100

    cleaning

--- src/PolskieUdemy/LE153_useHistory/Redux.js
+++ src/PolskieUdemy/LE153_useHistory/Redux.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom'
-const Redux = () => {
-    const history = useHistory();
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-    const handleOnClick = ()=> history.goBack();
-    console.log(history);
-    return ( 
-        <article>
-            <h2>Redux</h2>
-            <button onClick={handleOnClick} className='btn_mar'>wroc do poprzedniej strony</button>
-        </article>
-     );
-}
- 
+const Redux = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => history.goBack();
+  console.log(history);
+  return (
+    <article>
+      <h2>Redux</h2>
+      <button onClick={handleOnClick} className="btn_mar">
+        wroc do poprzedniej strony
+      </button>
+    </article>
+  );
+};
+
 export default Redux;
