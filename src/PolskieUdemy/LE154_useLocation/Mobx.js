89bfc82fb8546284587f4ba5d7d24385584b847c
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-22 13:45:04 +0100

    LE155 useParams

--- src/PolskieUdemy/LE154_useLocation/Mobx.js
+++ src/PolskieUdemy/LE154_useLocation/Mobx.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 const Mobx = () => {
-    return ( 
-        <article>
-            <h2>Mobx</h2>
-        </article>
-     );
-}
- 
+  const [inputData, setInputData] = useState("");
+  const history = useHistory();
+
+  const handleOnChange = (e) => setInputData(e.target.value);
+
+  const handleOnClick = () => {
+    const location = {
+      pathname: `/typescript/${inputData}`,
+    };
+    history.push(location);
+  };
+  return (
+    <article>
+      <h2>Mobx</h2>
+      <input onChange={handleOnChange} type="text" value={inputData} />
+      <button onClick={handleOnClick} className="btn_mar">
+        wyslij params do TypeScript
+      </button>
+    </article>
+  );
+};
+
 export default Mobx;
--- src/PolskieUdemy/LE154_useLocation/TypeScript.js
+++ src/PolskieUdemy/LE154_useLocation/TypeScript.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
 
 const TypeScript = () => {
+    const { message } = useParams();
+
+    // console.log(paramsObject);
     return ( 
         <article>
             <h2>TypeScript</h2>
+            <p>{message}</p>
         </article>
      );
 }
