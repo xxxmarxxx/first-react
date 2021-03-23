d86bbed0a5ba3a77553907302f5a163e4c6bcbd3
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-23 12:33:07 +0100

    add List

--- src/PolskieUdemy/Redux_LE156_166/App.js
+++ src/PolskieUdemy/Redux_LE156_166/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 
 import Form from './Form';
+import List from './List';
 import store from "./store/store";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <div className="center">
        <Form />
         <p>Programowanie z redux</p>
+        <List />
       </div>
     </Provider>
   );
