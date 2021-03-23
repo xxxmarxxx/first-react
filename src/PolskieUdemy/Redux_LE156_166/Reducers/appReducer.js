eebb126f794b8635e403ee10c6a6f7de49a32d57
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-23 11:44:56 +0100

    create file

---
+++ src/PolskieUdemy/Redux_LE156_166/Reducers/appReducer.js
@@ -0,0 +1,29 @@
+import { ADD, EDIT, DELETE } from "../Actions/appActions";
+
+export const appReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.payload];
+    case EDIT:
+      return state.map((currentStateElement) => {
+        if (currentStateElement.id !== action.payload.id) {
+          return currentStateElement;
+        }
+        const { author, comment, rate } = action.payload;
+
+        return {
+          author: author,
+          comment: comment,
+          id: currentStateElement.id,
+          rate,
+        };
+      });
+    case DELETE:
+      return state.filter(currentStateElement => currentStateElement.id !== action.payload.id);
+
+    default:
+      console.log(`Nie mamy action typ; ${action.type}`);
+
+      return state;
+  }
+};
