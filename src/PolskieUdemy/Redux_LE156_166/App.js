45acdc10ba9939a564a0e9225b8911de24afa047
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-24 12:44:39 +0100

    change connect to useDespatch

--- src/PolskieUdemy/Redux_LE156_166/App.js
+++ src/PolskieUdemy/Redux_LE156_166/App.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 
 import Form from './Form';
 import List from './List';
+
 import store from "./store/store";
 
 const App = () => {
--- src/PolskieUdemy/Redux_LE156_166/Form.js
+++ src/PolskieUdemy/Redux_LE156_166/Form.js
@@ -1,51 +1,52 @@
 import React, { useState } from "react";
-import {connect} from 'react-redux';
+import { useDispatch } from "react-redux";
 
-import {addRate, editRate} from './Actions/appActions'
-const Form = ({ 
-    addRate,
-    editRate,
-    author = "", 
-    comment = "", 
-    callback, 
-    id, 
-    rate = 0,
- }) => {
+import { addRate, editRate } from "./Actions/appActions";
+
+const Form = ({ author = "", comment = "", callback, id, rate = 0 }) => {
+
   const [authorInput, setAuthorInput] = useState(author);
   const [rateInput, setRateInput] = useState(rate);
   const [commentInput, setCommentInput] = useState(comment);
 
-  const handleChangeAuthor = (event) => setAuthorInput(event.target.value);
+  const dispatch = useDispatch();
 
-  const handleChangeRate = (event) => setRateInput(event.target.value);
+  const handleChangeAuthor = (e) => setAuthorInput(e.target.value);
 
-  const handleCommentChange = (event) => setCommentInput(event.target.value);
+  const handleChangeRate = (e) => setRateInput(e.target.value);
 
-  const handleOnSubmit = event =>{
-      event.preventDefault()
-      // if(!authorInput.length){
-      //     return;
-      // }
-      const rateObject = {
-         author: authorInput,
-         comment: commentInput,
-         id,
-         rate: Number(rateInput), 
-      }
+  const handleCommentChange = (e) => setCommentInput(e.target.value);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    // if(!authorInput.length){
+    //     return;
+    // }
+    const rateObject = {
+      author: authorInput,
+      comment: commentInput,
+      id,
+      rate: Number(rateInput),
+    };
     //   console.log(rateObject);
-      id ? editRate(rateObject) : addRate(rateObject);
+    id ? dispatch(editRate(rateObject)) : dispatch(addRate(rateObject));
 
-      if(id){
-            callback()
-      }
-  }
+    if (id) {
+      callback();
+    }
+  };
 
   return (
     <form onSubmit={handleOnSubmit}>
+      <div className="box">
       <div>
         <label>
-          Author:
-          <input onChange={handleChangeAuthor} type="text" value={authorInput} />
+          Author: 
+          <input
+            onChange={handleChangeAuthor}
+            type="text"
+            value={authorInput}
+          />
         </label>
       </div>
       <div>
@@ -57,20 +58,19 @@ const Form = ({
       <div>
         <label>
           Komentarz:
-          <input onChange={handleCommentChange} type="text" value={commentInput} />
+          <input
+            onChange={handleCommentChange}
+            type="text"
+            value={commentInput}
+          />
         </label>
       </div>
+      </div>
       <button type="submit" className="btn_mar">
-          {id ? "edycja oceny" : "dodaj ocene"}
-          </button>
+        {id ? "edycja oceny" : "dodaj ocene"}
+      </button>
     </form>
   );
 };
 
-const connectActionToProps = ({
-    addRate,
-    editRate,
-})
-const FormConsumer = connect(null, connectActionToProps)(Form)
-
-export default FormConsumer;
+export default Form;
