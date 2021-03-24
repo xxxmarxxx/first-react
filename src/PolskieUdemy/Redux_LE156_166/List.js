a5930550b570a52b990a6e13b89824e540ee7299
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-24 16:01:48 +0100

    change to useSelector

--- src/PolskieUdemy/Redux_LE156_166/List.js
+++ src/PolskieUdemy/Redux_LE156_166/List.js
@@ -1,21 +1,40 @@
+// import React from "react";
+// import {connect} from 'react-redux';
+// import Element from './Element';
+
+//! dziala z connect
+// const List = ({ ratesInProps }) => {
+//   const ratesElements = ratesInProps.map((rate) => (
+//     <Element key={rate.id} {...rate} />
+//   ));
+//   return (
+//   <ul>
+//       {ratesElements}
+//   </ul>
+//   );
+// };
+// const connectReduxStateToProps = store => ({
+//     ratesInProps: store.rates,
+// })
+// const ListConsumer = connect(connectReduxStateToProps)(List)
+// export default ListConsumer;
+
+
+//* a tu z useSelector jest krotszy zapis
 import React from "react";
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 import Element from './Element';
 
-const List = ({ ratesInProps }) => {
-  const ratesElements = ratesInProps.map((rate) => (
+const List = () => {
+  const rates = useSelector(store => store.rates)
+  const ratesElements = rates.map((rate) => (
     <Element key={rate.id} {...rate} />
   ));
-
-  return <ul>
+  return (
+  <ul>
       {ratesElements}
-  </ul>;
+  </ul>
+  );
 };
 
-const connectReduxStateToProps = store => ({
-    ratesInProps: store.rates,
-})
-
-const ListConsumer = connect(connectReduxStateToProps)(List)
-
-export default ListConsumer;
+export default List;
