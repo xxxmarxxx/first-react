1d722090edb868bcc4dde52ac2fbbc7ef0ad1d7a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-25 13:37:44 +0100

    import Comments form StoreProvider

--- src/PolskieUdemy/MobX/App.js
+++ src/PolskieUdemy/MobX/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Comments from './Comments'
+import Form from './Form';
+import StoreProvider from './stores/StoreProvider';
 
 const App = () => {
     return ( 
+        <StoreProvider>
         <div>
-            <h1>Hello</h1>
-            <p>Start</p>
+            <h1>Opinie na temat naszej ksiazki o programowaniu</h1>
+            <Comments/>
+            <Form/>
+
         </div>
+        </StoreProvider>
      );
 }
  
